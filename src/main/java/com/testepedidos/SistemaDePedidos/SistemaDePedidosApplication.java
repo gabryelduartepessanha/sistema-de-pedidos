c9bc839eb11c80a1f855a254617af9f407a3c4df
Gabryel Duarte <gabryelduartepessanha@gmail.com>
2026-03-10 13:59:43 -0300

    Project created

---
+++ src/main/java/com/testepedidos/SistemaDePedidos/SistemaDePedidosApplication.java
@@ -0,0 +1,13 @@
+package com.testepedidos.SistemaDePedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDePedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDePedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testepedidos/SistemaDePedidos/SistemaDePedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testepedidos.SistemaDePedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDePedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

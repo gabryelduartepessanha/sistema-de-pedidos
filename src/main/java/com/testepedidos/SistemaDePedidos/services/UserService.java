0e08cbaea3d87b591718d49ed2be506ae072f5d5
Gabryel Duarte <gabryelduartepessanha@gmail.com>
2026-03-13 12:06:50 -0300

    User update

--- src/main/java/com/testepedidos/SistemaDePedidos/services/UserService.java
+++ src/main/java/com/testepedidos/SistemaDePedidos/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.awt.image.AreaAveragingScaleFilter;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,5 +31,17 @@ public class UserService {
 
     public void delete(Long id){
         repository.deleteById(id);
+    }
+
+    public User update(Long id, User obj){
+        User entity = repository.getReferenceById(id);
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj){
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
     }
 }
